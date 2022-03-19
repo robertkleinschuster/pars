9ba6c049598aeb71a0914867e2ac47619000c924
Robert Kleinschuster <robert@kleinschuster.de>
2022-03-19 10:42:28 +0100

    pr/config-test (#64)
    * WebApplicationTest should render valid html layout
    
    * ConfigTest should load development files
    
    * ConfigTest should load development files

--- test/Core/Application/Web/WebApplicationTest.php
+++ test/Core/Application/Web/WebApplicationTest.php
@@ -1,7 +1,25 @@
 <?php
+
 namespace ParsTest\Core\Application\Web;
 
-class WebApplicationTest extends \PHPUnit\Framework\TestCase
-{
+use Pars\Core\Application\Web\WebApplication;
+use Pars\Core\Emitter\SapiEmitter;
+use ParsTest\Core\Container\MockContainer;
+use ParsTest\Core\Emitter\MockSapiEmitter;
+use PHPUnit\Framework\TestCase;
 
+class WebApplicationTest extends TestCase
+{
+    public function testShouldRenderValidHtmlLayout()
+    {
+        $container = MockContainer::getInstance();
+        $app = new WebApplication();
+        $app->run();
+        /* @var MockSapiEmitter $emitter */
+        $emitter = $container->get(SapiEmitter::class);
+        $html = $emitter->getResponse()->getBody()->getContents();
+        $tidy = tidy_parse_string($html);
+        $errors = tidy_error_count($tidy) + tidy_warning_count($tidy);
+        $this->assertEquals(0, $errors, $tidy->errorBuffer ?? '');
+    }
 }
--- test/Core/Config/ConfigTest.php
+++ test/Core/Config/ConfigTest.php
@@ -1,7 +1,19 @@
 <?php
 namespace ParsTest\Core\Config;
 
+use Pars\Core\Config\Config;
+
 class ConfigTest extends \PHPUnit\Framework\TestCase
 {
+    public function testShouldLoadFilesFromDirectoryAndUseFilenamesAsKeys()
+    {
+        $config = new Config(__DIR__ . '/examples/production');
+        $this->assertEquals('production', $config->get('foo.bar'));
+    }
 
+    public function testShouldOverrideWithDevelopmentFiles()
+    {
+        $config = new Config(__DIR__ . '/examples/development');
+        $this->assertEquals('development', $config->get('foo.bar'));
+    }
 }
