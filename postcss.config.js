358b2e0e78183a0b4edbf31addd838e76fe486e4
robertkleinschuster <robert@kleinschuster.de>
2022-04-26 23:13:12 +0200

    postcss

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    'postcss-combine-media-query': {},
+    'postcss-combine-duplicated-selectors': { removeDuplicatedValues: true },
+    'postcss-preset-env': {}
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ Encore
     config.targets = 'defaults'
   })
   .enableSassLoader()
+  .enablePostCssLoader()
   .enableTypeScriptLoader()
   .enableIntegrityHashes(Encore.isProduction())
 
