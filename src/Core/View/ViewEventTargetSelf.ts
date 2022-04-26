67cbb5dfddd903a349e7200a5d00dededb9ceb41
robertkleinschuster <robert@kleinschuster.de>
2022-04-26 19:58:32 +0200

    refactored view events

--- src/Core/View/ViewEventTargetSelf.ts
+++ src/Core/View/ViewEventTargetSelf.ts
@@ -1,10 +1,11 @@
 import ViewEvent from './ViewEvent'
+import ViewState from './ViewState'
 
 export default class ViewEventTargetSelf extends ViewEvent {
   async trigger (event: Event) {
     event.stopImmediatePropagation()
     const response = await this.getResponse()
     this.getElementHelper().replaceClosest(response.document.documentElement, this.selector)
-    history.pushState(this.getState(), '', this.getUrl())
+    ViewState.push(this.getState())
   }
 }
